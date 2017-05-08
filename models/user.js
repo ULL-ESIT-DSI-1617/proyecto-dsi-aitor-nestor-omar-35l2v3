bfe02398df7c84175359ec931da1d5ca182e4519
Aitor Bernal <alu0100830064@ull.edu.es>
2017-05-08 10:30:10 +0100

    Subiendo autenticación con login

---
+++ models/user.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chinegua on 8/5/17.
+ */
+let mongoose = require('mongoose');
+let Schema = require('mongoose').Schema
+
+let User = new Schema({
+    name            :   String,
+    password        :   String
+
+});
+
+
+module.exports = mongoose.model('User', User);
