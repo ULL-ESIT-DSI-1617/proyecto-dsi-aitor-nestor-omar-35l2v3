e8b66d7df80250af511ab7cee0501bd4863d18bc
Aitor Bernal <alu0100830064@ull.edu.es>
2017-05-11 20:14:44 +0100

    Cambiando timeline

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
  * Created by chinegua on 8/5/17.
  */
 let mongoose = require('mongoose');
-let Schema = require('mongoose').Schema
+let Schema = require('mongoose').Schema;
 
 let User = new Schema({
     name            :   String,
