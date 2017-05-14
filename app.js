f6eaf892d5d70f7e7ce7e9b7abc3f34978c67416
Omar Mendo <omen.mesa20@gmail.com>
2017-05-14 12:46:07 +0100

    Cambios y redirección create

--- app.js
+++ app.js
@@ -305,7 +305,7 @@ app.post('/calendar/create',function (req,res) {
      evento.save(function(err) {
          console.log("Evento creado");
      });
-
+     res.redirect('/calendar')
 });
 
 app.get('/calendar/edit/:id',auth, function(req, res) {
@@ -324,7 +324,6 @@ app.post('/calendar/edit/:id',auth, function(req, res) {
     Event.findOneAndUpdate({_id: req.params.id},{$set:{title: req.body.title,date: req.body.date,hour: req.body.hour,description:req.body.description }},{new: true},function(err,dox){
         res.render('timeline');
     })
-
 });
 
 /*
